1d9af54bb44316ade3b5fe9e0511a4a52acd13b7
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-02-12 00:05:09 -0800

    All tests finished

--- lab-jessica/test/cowsay-ctrl-test.js
+++ lab-jessica/test/cowsay-ctrl-test.js
@@ -29,4 +29,59 @@ describe('Cowsay Controller', function() {
       });
     });
   });
+
+  describe('#updateCow()', () => {
+    it('should return a meow that says meow', () => {
+      const expected = '\n' + cowsay.say({text: 'meow', f: 'meow'});
+      const result = this.cowsayCtrl.updateCow('meow', 'meow');
+      expect(result).toBe(expected);
+    });
+  });
+
+  describe('#save()', () => {
+    it('should add a talking cowfile to the history array', () => {
+      const expected = '\n' + cowsay.say({text: 'test', f: 'chick'});
+      this.cowsayCtrl.text = 'test';
+      this.cowsayCtrl.cowfile = 'chick';
+      this.cowsayCtrl.save();
+      expect(this.cowsayCtrl.history.length).toBe(1);
+      expect(this.cowsayCtrl.history[0]).toBe(expected);
+    });
+
+    it('should set lastSave property to saved cow', () => {
+      const expected = '\n' + cowsay.say({text: 'test', f: 'chick'});
+      this.cowsayCtrl.text = 'test';
+      this.cowsayCtrl.cowfile = 'chick';
+      this.cowsayCtrl.save();
+      expect(this.cowsayCtrl.lastSave).toBe(expected);
+    });
+  });
+
+  describe('#undo()', () => {
+    it('should remove an item from the history array', () => {
+      const expected = '\n' + cowsay.say({text: 'test', f: 'chick'});
+      this.cowsayCtrl.text = 'test';
+      this.cowsayCtrl.cowfile = 'chick';
+      this.cowsayCtrl.save();
+      this.cowsayCtrl.text = 'test2';
+      this.cowsayCtrl.cowfile = 'meow';
+      this.cowsayCtrl.save();
+      expect(this.cowsayCtrl.history.length).toBe(2);
+      this.cowsayCtrl.undo();
+      expect(this.cowsayCtrl.history.length).toBe(1);
+      expect(this.cowsayCtrl.history[0]).toBe(expected);
+    });
+
+    it('should set lastSave property to next item in the history array', () => {
+      const expected = '\n' + cowsay.say({text: 'test', f: 'chick'});
+      this.cowsayCtrl.text = 'test';
+      this.cowsayCtrl.cowfile = 'chick';
+      this.cowsayCtrl.save();
+      this.cowsayCtrl.text = 'test2';
+      this.cowsayCtrl.cowfile = 'meow';
+      this.cowsayCtrl.save();
+      this.cowsayCtrl.undo();
+      expect(this.cowsayCtrl.lastSave).toBe(expected);
+    });
+  });
 });
