f52f600f47215af3ea68d12fe1ac0f16bf8e10f5
Michael Bishop <polishthebishop@gmail.com>
2017-02-12 15:20:13 -0800

    Something not right, reload some configs next

--- lab-michael/test/cowsay-ctrl-test.js
+++ lab-michael/test/cowsay-ctrl-test.js
@@ -2,3 +2,26 @@ require('./lib/test-helper.js');
 
 const angular = require('angular');
 const cowsay = require('cowsay-browser');
+
+describe('Cowsay Controller', function() {
+  beforeEach(function() {
+    angular.mock.module('cowsayDemoApp');
+    angular.mock.inject(function($controller)  {
+      this.cowsayCtrl = new $controller('CowsayController');
+    });
+  });
+  describe('initial properties from on-load', function() {
+    it('TITLE should read Welcome to CowTown', function() {
+      expect(this.cowsay.title).toBe('Welcome to CowTown');
+    });
+    it('HISTORY should have nothing in it', function() {
+      expect(Array.isArray(this.cowsayCtrl.history)).toBe(true);
+    });
+    // it('should show bonified "cow" files', function() {
+    //   cowsay.list((err, cows) => {
+    //     expect(this.cowsayCtrl.cows).toEqual(cows);
+    //     expect(this.cowsayCtrl.current).toEqual(cows[0]);
+    //   });
+    // });
+  });
+});
